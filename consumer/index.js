8312b69adf1a5076c757aecde3d16bd81fa2ac07
Amburi Roy <amburi.roy@gmail.com>
2024-03-18 22:11:42 +0100

    chore: improve variable names

--- consumer/index.js
+++ consumer/index.js
@@ -1,26 +1,27 @@
-require('dotenv').config();
-
 const amqp = require('amqplib/callback_api');
 
 const rabbitmqHost = process.env.RABBITMQ_HOST || 'rabbitmq';
 
-amqp.connect(`amqp://${rabbitmqHost}`, (err, conn) => {
-    if (err) {
-        console.error('Error connecting to RabbitMQ:', err);
+
+amqp.connect(`amqp://${rabbitmqHost}`, (error, connection) => {
+    if (error) {
+        console.error('Error connecting to RabbitMQ:', error);
         process.exit(1);
     }
 
-    conn.createChannel((err, ch) => {
-        if (err) {
-            console.error('Error creating channel:', err);
+    connection.createChannel((error, channel) => {
+        if (error) {
+            console.error('Error creating channel:', error);
         } else {
-            const queue = process.env.RANDOM_NUMBER_QUEUE_NAME;
-            ch.assertQueue(queue, { durable: false });
+
+            const queue = process.env.MESSAGE_QUEUE;
+
+            channel.assertQueue(queue, { durable: false });
 
             console.log('Waiting for messages...');
 
-            ch.consume(queue, (msg) => {
-                console.log('Received message:', msg.content.toString());
+            channel.consume(queue, (message) => {
+                console.log('Consuming Enriched Number:', message.content.toString());
             }, { noAck: true });
         }
     });
--- publisher/index.js
+++ publisher/index.js
@@ -2,23 +2,24 @@ const amqp = require('amqplib/callback_api');
 
 const rabbitmqHost = process.env.RABBITMQ_HOST || 'rabbitmq';
 
-amqp.connect(`amqp://${rabbitmqHost}`, (err, conn) => {
-    if (err) {
-        console.error('Error connecting to RabbitMQ:', err);
+amqp.connect(`amqp://${rabbitmqHost}`, (error, connection) => {
+    if (error) {
+        console.error('Error connecting to RabbitMQ:', error);
         process.exit(1);
     }
 
-    conn.createChannel((err, ch) => {
-        if (err) {
-            console.error('Error creating channel:', err);
+    connection.createChannel((error, channel) => {
+        if (error) {
+            console.error('Error creating channel:', error);
         } else {
-            const queue = process.env.RANDOM_NUMBER_QUEUE_NAME;
-            ch.assertQueue(queue, { durable: false });
+            const queue = process.env.RANDOM_NUMBER_QUEUE;
+
+            channel.assertQueue(queue, { durable: false });
 
             setInterval(() => {
                 const message = Math.random().toString();
-                console.log('Sending message:', message);
-                ch.sendToQueue(queue, Buffer.from(message));
+                console.log('Publishing Random Number:', message);
+                channel.sendToQueue(queue, Buffer.from(message));
             }, 1000); // Send a message every second
         }
     });
